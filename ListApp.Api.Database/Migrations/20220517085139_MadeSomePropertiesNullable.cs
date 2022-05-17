823f0f6c81d02d12f9cf2f4d85311930c0ec4de4
Jefferson Amori da Rocha <jefferson_adr@hotmail.com>
2022-05-17 09:52:36 +0100

    Made some properties on ListItem nullable

---
+++ ListApp.Api.Database/Migrations/20220517085139_MadeSomePropertiesNullable.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ListApp.Api.Database.Migrations
+{
+    public partial class MadeSomePropertiesNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Text",
+                table: "ListItems",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "ListItems",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Text",
+                table: "ListItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "ListItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
--- ListApp.Api.Database/Models/ListItem.cs
+++ ListApp.Api.Database/Models/ListItem.cs
@@ -5,8 +5,8 @@
         public long Id { get; set; }
         public string Guid { get; set; }
         public bool Checked { get; set; }
-        public string Text { get; set; }
-        public string Description { get; set; }
+        public string? Text { get; set; }
+        public string? Description { get; set; }
         public long ListId { get; set; }
         public int Index { get; set; }
         public bool IsDeleted { get; set; }
