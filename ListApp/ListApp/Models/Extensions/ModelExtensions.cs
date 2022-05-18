235c2b309b6e752ef5b6eed14424bd0d7c417d37
Jefferson Amori da Rocha <jefferson_adr@hotmail.com>
2022-05-18 15:00:18 +0100

    Refactored Sync code + added ActivityIndicator to Accounts page

---
+++ ListApp/ListApp/Models/Extensions/ModelExtensions.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Linq;
+using ApiModel = ListApp.Api.Models;
+
+namespace ListApp.Models.Extensions
+{
+    public static class ModelExtensions
+    {
+        public static List ToLocalModel(this ApiModel.List cloudList)
+        {
+            var list = new List
+            {
+                ListId = cloudList.Guid,
+                Name = cloudList.Name,
+                IsDeleted = cloudList.IsDeleted,
+                Index = cloudList.Index,
+                CreationDate = cloudList.CreationDate,
+                LastChangedDate = DateTime.UtcNow
+            };
+
+            foreach (var cloudListItem in cloudList.ListItems)
+            {
+                list.ListItems.Add(cloudListItem.ToLocalModel());
+            }
+
+            return list;
+        }
+        public static ListItem ToLocalModel(this ApiModel.ListItem cloudListItem)
+        {
+            var localListItem = new ListItem
+            {
+                Id = cloudListItem.Guid,
+                Text = cloudListItem.Text,
+                Description = cloudListItem.Description,
+                Index = cloudListItem.Index,
+                IsDeleted = cloudListItem.IsDeleted,
+                Checked = cloudListItem.Checked,
+                CreationDate = cloudListItem.CreationDate,
+                LastChangedDate = DateTime.UtcNow
+            };
+
+            return localListItem;
+        }
+        public static ApiModel.ListItem ToApiModel(this ListItem localListItem, long apiListId = 0L)
+        {
+            var cloudListItem = new ApiModel.ListItem
+            {
+                ListId = apiListId,
+                Guid = localListItem.Id,
+                Text = localListItem.Text,
+                Description = localListItem.Description,
+                Index = localListItem.Index,
+                IsDeleted = localListItem.IsDeleted,
+                Checked = localListItem.Checked,
+                CreationDate = localListItem.CreationDate,
+                LastChangedDate = DateTime.UtcNow
+            };
+
+            return cloudListItem;
+        }
+
+        public static ApiModel.List ToApiModel(this List localList)
+        {
+            var cloudList = new ApiModel.List();
+            cloudList.Guid = localList.ListId;
+            cloudList.Name = localList.Name;
+            cloudList.IsDeleted = localList.IsDeleted;
+            cloudList.Index = localList.Index;
+            cloudList.OwnerEmail = ApplicationUser.Current.Email;
+            cloudList.CreationDate = localList.CreationDate;
+            cloudList.LastChangedDate = DateTime.UtcNow;
+
+            foreach (var localListItem in localList.ListItems)
+            {
+                cloudList.ListItems.Add(localListItem.ToApiModel());
+            }
+
+            return cloudList;
+        }
+        public static void CopyFrom(this ApiModel.List cloudList, List localList)
+        {
+            cloudList.Name = localList.Name;
+            cloudList.IsDeleted = localList.IsDeleted;
+            cloudList.Index = localList.Index;
+
+            foreach (var localListItem in localList.ListItems)
+            {
+                if (cloudList.ListItems.FirstOrDefault(x => x.Guid == localListItem.Id) is ApiModel.ListItem cloudListItem)
+                {
+                    cloudListItem.IsDeleted = localListItem.IsDeleted;
+                    cloudListItem.Text = localListItem.Text;
+                    cloudListItem.Checked = localListItem.Checked;
+                    cloudListItem.Description = localListItem.Description;
+                    cloudListItem.Index = localListItem.Index;
+                    cloudListItem.LastChangedDate = DateTime.UtcNow;
+                }
+                else
+                {
+                    cloudList.ListItems.Add(localListItem.ToApiModel(cloudList.Id));
+                }
+            }
+        }
+    }
+}
--- ListApp/ListApp/ViewModels/Settings/AccountManagementViewModel.cs
+++ ListApp/ListApp/ViewModels/Settings/AccountManagementViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using ListApp.Helpers;
 using ListApp.Models;
+using ListApp.Models.Extensions;
 using ListApp.Resources;
 using ListApp.Services.Interfaces;
 using Microsoft.AppCenter.Crashes;
@@ -21,20 +22,20 @@ namespace ListApp.ViewModels.Settings
     public class AccountManagementViewModel : BaseViewModel
     {
         private ILogger _logger = DependencyService.Get<ILogger>();
-        private IDataStore<List> DataStore = DependencyService.Get<IDataStore<List>>();
-        public IDialogService DialogService => DependencyService.Get<IDialogService>();
+        private IDataStore<List> _dataStore = DependencyService.Get<IDataStore<List>>();
+        private IDialogService _dialogService => DependencyService.Get<IDialogService>();
         public ICommand LoginWithGoogleCommand { get; }
         public ICommand SyncCommand { get; }
         public ICommand SignOutCommand { get; }
+        private bool _isSyncing;
 
-        private string _token;
 
-        public string Token
+        public bool IsSyncing
         {
-            get { return _token; }
+            get { return _isSyncing; }
             set
             {
-                _token = value;
+                _isSyncing = value;
                 OnPropertyChanged();
             }
         }
@@ -71,54 +72,30 @@ namespace ListApp.ViewModels.Settings
 
         private async void OnSyncCommand()
         {
+            IsSyncing = true;
+
             try
             {
-                var syncStartedDialogTask = DialogService.DisplayToastAsync("Syncing...");
+                var syncStartedDialogTask = _dialogService.DisplayToastAsync("Syncing...");
 
                 HttpClient httpClient = new HttpClient();
                 httpClient.BaseAddress = Secrets.ListFreakApiEndpoint;
 
-                var allBackedupListsForCurrentUserRequest = await httpClient.GetAsync($"lists/ownerEmail/{ApplicationUser.Current.Email}");
-                var allBackedupListsForCurrentUser = await JsonSerializer.DeserializeAsync<List<ApiModel.List>>(await allBackedupListsForCurrentUserRequest.Content.ReadAsStreamAsync());
+                // Start 2 tasks: 1) to retrieve data from local DB and 2) to request the online lists to the API
+                var allBackedupListsForCurrentUserRequestTask = httpClient.GetAsync($"lists/ownerEmail/{ApplicationUser.Current.Email}");
+                var localListsTask = _dataStore.GetItemsAsync();
 
-                var localLists = await DataStore.GetItemsAsync();
+                var localLists = await localListsTask;
+                var allBackedupListsForCurrentUserRequest = await allBackedupListsForCurrentUserRequestTask;
+                var allBackedupListsForCurrentUser = await JsonSerializer.DeserializeAsync<List<ApiModel.List>>(await allBackedupListsForCurrentUserRequest.Content.ReadAsStreamAsync());
 
                 foreach (var cloudList in allBackedupListsForCurrentUser)
                 {
                     if (localLists.FirstOrDefault(x => x.ListId == cloudList.Guid) is List localList)
                     {
-                        // Update the list on the cloud.
-                        cloudList.Name = localList.Name;
-                        cloudList.IsDeleted = localList.IsDeleted;
-                        cloudList.Index = localList.Index;
+                        // Update the cloud list based on local data.
+                        cloudList.CopyFrom(localList);
                         cloudList.LastChangedDate = DateTime.UtcNow;
-
-                        foreach (var localListItem in localList.ListItems)
-                        {
-                            if (cloudList.ListItems.FirstOrDefault(x => x.Guid == localListItem.Id) is ApiModel.ListItem cloudListItem)
-                            {
-                                cloudListItem.IsDeleted = localListItem.IsDeleted;
-                                cloudListItem.Text = localListItem.Text;
-                                cloudListItem.Checked = localListItem.Checked;
-                                cloudListItem.Description = localListItem.Description;
-                                cloudListItem.Index = localListItem.Index;
-                                cloudListItem.LastChangedDate = DateTime.UtcNow;
-                            }
-                            else
-                            {
-                                var listItem = new ApiModel.ListItem();
-                                listItem.ListId = cloudList.Id;
-                                listItem.Guid = localListItem.Id;
-                                listItem.Text = localListItem.Text;
-                                listItem.Description = localListItem.Description;
-                                listItem.Index = localListItem.Index;
-                                listItem.IsDeleted = localListItem.IsDeleted;
-                                listItem.Checked = localListItem.Checked;
-                                listItem.CreationDate = localListItem.CreationDate;
-                                listItem.LastChangedDate = DateTime.UtcNow;
-                                cloudList.ListItems.Add(listItem);
-                            }
-                        }
 
                         string jsonContent = JsonSerializer.Serialize(cloudList);
                         var response = await httpClient.PutAsync($"lists/{cloudList.Id}", new StringContent(jsonContent, Encoding.UTF8, "application/json"));
@@ -126,29 +103,7 @@ namespace ListApp.ViewModels.Settings
                     else
                     {
                         // Insert the list from the cloud into the local DB.
-                        var list = new List();
-                        list.ListId = cloudList.Guid;
-                        list.Name = cloudList.Name;
-                        list.IsDeleted = cloudList.IsDeleted;
-                        list.Index = cloudList.Index;
-                        list.CreationDate = cloudList.CreationDate;
-                        list.LastChangedDate = DateTime.UtcNow;
-
-                        foreach (var cloudListItem in cloudList.ListItems)
-                        {
-                            var localListItem = new ListItem();
-                            localListItem.Id = cloudListItem.Guid;
-                            localListItem.Text = cloudListItem.Text;
-                            localListItem.Description = cloudListItem.Description;
-                            localListItem.Index = cloudListItem.Index;
-                            localListItem.IsDeleted = cloudListItem.IsDeleted;
-                            localListItem.Checked = cloudListItem.Checked;
-                            localListItem.CreationDate = cloudListItem.CreationDate;
-                            localListItem.LastChangedDate = DateTime.UtcNow;
-                            list.ListItems.Add(localListItem);
-                        }
-
-                        await DataStore.AddItemAsync(list);
+                        await _dataStore.AddItemAsync(cloudList.ToLocalModel());
                     }
                 }
 
@@ -156,39 +111,20 @@ namespace ListApp.ViewModels.Settings
                 {
                     if (!allBackedupListsForCurrentUser.Any(x => x.Guid == localList.ListId))
                     {
-                        var cloudList = new ApiModel.List();
-                        cloudList.Guid = localList.ListId;
-                        cloudList.Name = localList.Name;
-                        cloudList.IsDeleted = localList.IsDeleted;
-                        cloudList.Index = localList.Index;
-                        cloudList.OwnerEmail = ApplicationUser.Current.Email;
-                        cloudList.CreationDate = localList.CreationDate;
-                        cloudList.LastChangedDate = DateTime.UtcNow;
-
-                        foreach (var localListItem in localList.ListItems)
-                        {
-                            var cloudListItem = new ApiModel.ListItem();
-                            cloudListItem.Guid = localListItem.Id;
-                            cloudListItem.Text = localListItem.Text;
-                            cloudListItem.Description = localListItem.Description;
-                            cloudListItem.Index = localListItem.Index;
-                            cloudListItem.IsDeleted = localListItem.IsDeleted;
-                            cloudListItem.Checked = localListItem.Checked;
-                            cloudListItem.CreationDate = localListItem.CreationDate;
-                            cloudListItem.LastChangedDate = DateTime.UtcNow;
-                        }
-
+                        var cloudList = localList.ToApiModel();
                         var response = await httpClient.PostAsync($"lists", new StringContent(JsonSerializer.Serialize(cloudList), Encoding.UTF8, "application/json"));
                     }
                 }
 
-                var syncFinishedDialogTask = DialogService.DisplayToastAsync("Synced!");
-
-                await Task.WhenAll(syncStartedDialogTask, syncFinishedDialogTask);
+                var syncFinishedDialogTask = _dialogService.DisplayToastAsync("Synced!");
             }
             catch (Exception ex)
             {
                 Crashes.TrackError(ex);
+            }
+            finally
+            {
+                IsSyncing = false;
             }
         }
 
@@ -198,7 +134,7 @@ namespace ListApp.ViewModels.Settings
             {
                 ApplicationUser.Current.Unset();
                 Preferences.Remove(PreferencesKeys.ApplicationUserInfo);
-                await DialogService.DisplayToastAsync("Signed out");
+                await _dialogService.DisplayToastAsync("Signed out");
             }
             catch (Exception ex)
             {
