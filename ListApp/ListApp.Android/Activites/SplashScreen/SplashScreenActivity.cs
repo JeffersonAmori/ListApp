375f9ca797e723d7bb48d4918efcc0bcb9eefe6c
Jefferson Amori da Rocha <jefferson_adr@hotmail.com>
2022-05-29 18:22:24 +0100

    Implemented splash screen

---
+++ ListApp/ListApp.Android/Activites/SplashScreen/SplashScreenActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using AndroidX.AppCompat.App;
+
+namespace ListApp.Droid.Activites.SplashScreen
+{
+    [Activity(Theme = "@style/SplashScreenTheme", MainLauncher = true, NoHistory = true)]
+    public class SplashScreenActivity : AppCompatActivity
+    {
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
+        public override void OnBackPressed() { }
+    }
+}
